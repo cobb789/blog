4783bd0633ddf672b56290dbaf3026e737712bc6
larrykoo <larry.koo711@gmail.com>
2026-02-06 22:12:52 +0800

    fix: 修复构建问题 (postcss注释/lucide-react/contentlayer)

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,4 @@
-# https://github.com/shadcn-ui/taxonomy/blob/main/postcss.config.js
+// https://github.com/shadcn-ui/taxonomy/blob/main/postcss.config.js
 module.exports = {
   plugins: {
     tailwindcss: {},
